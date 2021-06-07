2e91cb775e90b77f25a6c4c8152de4cc96c9f492
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2021-06-07 18:45:11 +0300

    Update CacheAspect.cs
    Update 07.06.2021

--- Core/Aspects/Autofac/Caching/CacheAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -21,11 +21,11 @@ namespace Core.Aspects.Autofac.Caching
             _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
         }
 
-        public override void Intercept(IInvocation invocation)
+         public override void Intercept(IInvocation invocation)
         {
-            var methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
-            var arguments = invocation.Arguments.ToList();
-            var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
+            var methodName = string.Format($"{invocation.Arguments[0]}.{invocation.Method.Name}");
+            var arguments = invocation.Arguments;
+            var key = $"{methodName}({BuildKey(arguments)})";
             if (_cacheManager.IsAdd(key))
             {
                 invocation.ReturnValue = _cacheManager.Get(key);
@@ -33,6 +33,18 @@ namespace Core.Aspects.Autofac.Caching
             }
             invocation.Proceed();
             _cacheManager.Add(key, invocation.ReturnValue, _duration);
+        }
+
+        string BuildKey(object[] args)
+        {
+            var sb = new StringBuilder();
+            foreach (var arg in args)
+            {
+                var paramValues = arg.GetType().GetProperties().Select(p => p.GetValue(arg)?.ToString() ?? string.Empty);
+                sb.Append(string.Join('_', paramValues));
+
+            }
+            return sb.ToString();
         }
     }
 }
